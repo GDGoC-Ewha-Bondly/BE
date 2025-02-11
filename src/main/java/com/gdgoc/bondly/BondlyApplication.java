a8dda4f09d2e77c2dca88b29ac51ae43f6aa3c82
jiu31 <jiu.jung.cs@gmail.com>
2025-02-11 15:15:29 +0900

    initial upload

---
+++ src/main/java/com/gdgoc/bondly/BondlyApplication.java
@@ -0,0 +1,13 @@
+package com.gdgoc.bondly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdgoc/bondly/BondlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdgoc.bondly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
